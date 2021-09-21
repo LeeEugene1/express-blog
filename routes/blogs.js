bf485e2005288dafd1ed55e9f9e121d5145ff635
eugene <uwm1004@gmail.com>
2021-09-21 17:51:04 +0900

    route

---
+++ routes/blogs.js
@@ -0,0 +1,10 @@
+//blog routes
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res)=>{
+    res.send('this is the blogs route')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const blogRouter = require('./routes/blogs')
 
 // set template engine
 app.set('view engine', 'ejs')
@@ -7,5 +8,7 @@ app.set('view engine', 'ejs')
 app.get('/',(res,req)=>{
     req.render('index')
 })
+
+app.use('/blogs',blogRouter)
 
 app.listen(5000)
